8af0378e71691f59398e3f72bedd2fce48a7c2b9
최석원 <bluecircle2290@gmail.com>
2023-06-16 11:21:59 +0900

    Mock기반 testcode 추가

--- src/test/java/com/example/junitstudy/JunitstudyApplicationTests.java
+++ src/test/java/com/example/junitstudy/JunitstudyApplicationTests.java
@@ -4,14 +4,28 @@ import com.example.junitstudy.calculator.Calculator;
 import com.example.junitstudy.calculator.DollarCalculator;
 import com.example.junitstudy.calculator.MarketApi;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class JunitstudyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Mock
+	public MarketApi marketApi;
+
+	@BeforeEach
+	public void init(){
+		Mockito.lenient().when(marketApi.connect()).thenReturn(3000);
 	}
 
 	@Test
@@ -28,6 +42,18 @@ class JunitstudyApplicationTests {
 		Calculator calculator = new Calculator(dollarCalculator);
 		System.out.println(calculator.sum(10, 10));
 		Assertions.assertEquals(22000, calculator.sum(10, 10));
+		Assertions.assertEquals(0, calculator.minus(10, 10));
+	}
+
+	@Test
+	public void mocktest(){
+		DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+		dollarCalculator.init();
+
+		Calculator calculator = new Calculator(dollarCalculator);
+		System.out.println(calculator.sum(10, 10));
+		Assertions.assertEquals(60000, calculator.sum(10, 10));
+		Assertions.assertEquals(0, calculator.minus(10, 10));
 	}
 
 }
